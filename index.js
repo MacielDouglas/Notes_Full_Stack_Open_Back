f32b562ca28d043d1c11b5eb35c989f8020901b1
MacielDouglas <douglasmaciel.dm@gmail.com>
2023-04-21 11:54:07 -0300

    Criado middlewares para gerenciar erros e terminado os métodos PUT e DELETE

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
+require('dotenv').config();
 
 const Note = require('./models/note');
 
@@ -14,6 +14,16 @@ const requestLogger = (request, response, next) => {
   next();
 };
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+
+  next(error);
+};
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' });
 };
@@ -23,36 +33,37 @@ app.use(express.json());
 app.use(requestLogger);
 app.use(express.static('build'));
 
-let notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    important: true,
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only JavaScript',
-    important: false,
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    important: true,
-  },
-  {
-    id: 4,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    important: true,
-  },
-  {
-    id: 5,
-    content:
-      'GET and POST are the most important methods of HTTP protoadfafasfdasdfadsfcol',
-    important: true,
-  },
-];
+// let notes = [
+//   {
+//     id: 1,
+//     content: 'HTML is easy',
+//     important: true,
+//   },
+//   {
+//     id: 2,
+//     content: 'Browser can execute only JavaScript',
+//     important: false,
+//   },
+//   {
+//     id: 3,
+//     content: 'GET and POST are the most important methods of HTTP protocol',
+//     important: true,
+//   },
+//   {
+//     id: 4,
+//     content: 'GET and POST are the most important methods of HTTP protocol',
+//     important: true,
+//   },
+//   {
+//     id: 5,
+//     content:
+//       'GET and POST are the most important methods of HTTP protoadfafasfdasdfadsfcol',
+//     important: true,
+//   },
+// ];
 
 // Gerenciador de eventos que lida com requisiçoes HTTP GET
+
 app.get('/api/notes', (request, response) => {
   Note.find({}).then((notes) => {
     response.json(notes);
@@ -79,21 +90,46 @@ app.post('/api/notes', (request, response) => {
 });
 
 // Esse app ira gerenciar todas as requisições HTTP GET com parametro id
-
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then((note) => {
-    response.json(note);
-  });
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id);
-  notes = notes.filter((note) => note.id !== id);
-
-  response.status(204).end();
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body;
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  };
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
+});
+
+// gerenciador de requisições com um endpoint desconhecido
 app.use(unknownEndpoint);
+
+// gerenciador de requisições com um resultado para erros
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
