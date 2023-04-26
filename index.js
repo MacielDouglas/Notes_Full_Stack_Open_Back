29d32afd5881799a3f6336255a5fc3babb0d44e5
MacielDouglas <douglasmaciel.dm@gmail.com>
2023-04-26 08:13:43 -0300

    Reorganizado o cod, movendo errorHandler e unknownEndpoint para o final do codigo.

--- index.js
+++ index.js
@@ -14,55 +14,10 @@ const requestLogger = (request, response, next) => {
   next();
 };
 
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
-
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' });
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message });
-  }
-
-  next(error);
-};
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' });
-};
-
 app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
 app.use(express.static('build'));
-
-// let notes = [
-//   {
-//     id: 1,
-//     content: 'HTML is easy',
-//     important: true,
-//   },
-//   {
-//     id: 2,
-//     content: 'Browser can execute only JavaScript',
-//     important: false,
-//   },
-//   {
-//     id: 3,
-//     content: 'GET and POST are the most important methods of HTTP protocol',
-//     important: true,
-//   },
-//   {
-//     id: 4,
-//     content: 'GET and POST are the most important methods of HTTP protocol',
-//     important: true,
-//   },
-//   {
-//     id: 5,
-//     content:
-//       'GET and POST are the most important methods of HTTP protoadfafasfdasdfadsfcol',
-//     important: true,
-//   },
-// ];
 
 // Gerenciador de eventos que lida com requisiçoes HTTP GET
 
@@ -124,8 +79,24 @@ app.put('/api/notes/:id', (request, response, next) => {
     .catch((error) => next(error));
 });
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+
 // gerenciador de requisições com um endpoint desconhecido
 app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
 
 // gerenciador de requisições com um resultado para erros
 app.use(errorHandler);
