dda7d8a710a8cc7042d0b91e931aa2f4da3983e5
MacielDouglas <douglasmaciel.dm@gmail.com>
2023-05-15 10:58:25 -0300

    Criado users e suas rotas, definido como usuarios unicos, usando hash

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -1,4 +1,7 @@
 // o pacote supertest para nos ajudar a escrever os testes para nossa API.
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
+
 const supertest = require('supertest');
 const mongoose = require('mongoose');
 const helper = require('./test_helper');
@@ -119,6 +122,61 @@ describe('deletion of a note', () => {
     const contents = notesAtEnd.map((r) => r.content);
 
     expect(contents).not.toContain(noteToDelete.content);
+  });
+});
+
+describe('when there is initially one user in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({});
+
+    const passwordHash = await bcrypt.hash('sekret', 10);
+    const user = new User({ username: 'root', passwordHash });
+
+    await user.save();
+  });
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = {
+      username: 'mluukkai',
+      name: 'Matti Luukkainen',
+      password: 'salainen',
+    };
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
+
+    const usersAtEnd = await helper.usersInDb();
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1);
+
+    const usernames = usersAtEnd.map((u) => u.username);
+    expect(usernames).toContain(newUser.username);
+  });
+
+  // verifique se um novo usuário com o mesmo nome de usuário não pode ser criado
+  test('creation fails with proper statuscode and message if username already taken', async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = {
+      username: 'root',
+      name: 'Superuser',
+      password: 'salainen',
+    };
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/);
+
+    expect(result.body.error).toContain('expected `username` to be unique');
+
+    const usersAtEnd = await helper.usersInDb();
+    expect(usersAtEnd).toEqual(usersAtStart);
   });
 });
 
