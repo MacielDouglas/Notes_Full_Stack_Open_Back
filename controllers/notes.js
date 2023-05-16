57650223f51e47b09408f18516d6ce22112740bf
MacielDouglas <douglasmaciel.dm@gmail.com>
2023-05-16 15:38:23 -0300

    revertido para o commit clean

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,58 +1,57 @@
-const notesRouter = require('express').Router();
-const Note = require('../models/note');
-const User = require('../models/user');
+const notesRouter = require('express').Router()
+const Note = require('../models/note')
 
 notesRouter.get('/', async (request, response) => {
+<<<<<<< HEAD
   const notes = await Note.find({}).populate('user', { username: 1, name: 1 });
 
   response.json(notes);
 });
+=======
+  const notes = await Note.find({})
+  response.json(notes)
+})
+>>>>>>> parent of b477691 (Resolvido problema enviar post com usuario não era reconhecido)
 
 notesRouter.post('/', async (request, response) => {
-  const body = request.body;
-
-  const user = await User.findById(body.userId);
+  const body = request.body
 
   const note = new Note({
     content: body.content,
-    important: body.important === undefined ? false : body.important,
-    user: user.id,
-  });
+    important: body.important || false,
+  })
 
-  const savedNote = await note.save();
-  user.notes = user.notes.concat(savedNote._id);
-  await user.save();
-
-  response.json(savedNote);
-});
+  const savedNote = await note.save()
+  response.status(201).json(savedNote)
+})
 
 notesRouter.get('/:id', async (request, response) => {
-  const note = await Note.findById(request.params.id);
+  const note = await Note.findById(request.params.id)
   if (note) {
-    response.json(note);
+    response.json(note)
   } else {
-    response.status(404).end();
+    response.status(404).end()
   }
-});
+})
 
 notesRouter.delete('/:id', async (request, response) => {
-  await Note.findByIdAndRemove(request.params.id);
-  response.status(204).end();
-});
+  await Note.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
 
 notesRouter.put('/:id', (request, response, next) => {
-  const body = request.body;
+  const body = request.body
 
   const note = {
     content: body.content,
     important: body.important,
-  };
+  }
 
   Note.findByIdAndUpdate(request.params.id, note, { new: true })
-    .then((updatedNote) => {
-      response.json(updatedNote);
+    .then(updatedNote => {
+      response.json(updatedNote)
     })
-    .catch((error) => next(error));
-});
+    .catch(error => next(error))
+})
 
-module.exports = notesRouter;
+module.exports = notesRouter
